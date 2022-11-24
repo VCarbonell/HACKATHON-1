c8ec9f4d88db0e91d6715f7c1346e4976463be3e
ledez gregory <ledez.greg@gmail.com>
2022-11-24 16:59:24 +0100

    finito fond d'écran

--- src/main.js
+++ src/main.js
@@ -6,28 +6,62 @@ const playBtn = document.querySelector('.playBtn');
 const bgTl = gsap.timeline();
 
 
- 
+ gsap.to('.game__sprite',{
+   y:'10px',
+   repeat:-1,
+   yoyo:true
+ })
 const play = () => {
-
-
-  bgTl.to('.levelOne', {
-    x:'-100%',
-    duration:10,
-    delay:1,
-    ease:'linear'
-
-  })
-.to('.obstacle', {
-  x:'-2000px',
-  duration:4
-}, 2)
-  .to('.levelTwo', {
-    x:'-200%',
-    duration:8,
-    ease:'linear'
-  }, 4)
   bgTl.paused( !bgTl.paused());
 playBtn.innerHTML = bgTl.paused() ? 'Start' : 'Pause'
+
+  bgTl.to('.levelOne', {
+    x:'-20%',
+    duration:20,
+    delay:1,
+    ease:'linear',
+
+  })
+  .to('.levelOne', {
+    x:'-40%',
+    duration:10,
+    ease:'linear',
+
+  })
+.to('.obstacle, .obstacle2', {
+  x:'-2000px',
+  duration:4
+}, 3)
+.to('.levelOne', {
+  x:'-60%',
+  duration:8,
+  ease:'linear',
+
+})
+.to('.levelOne', {
+  x:'-80%',
+  duration:8,
+  ease:'linear',
+
+})
+.to('.levelOne', {
+  x:'-95%',
+  duration:8,
+  ease:'linear',
+  
+
+
+})
+.to('.congrats',{
+  opacity:0.5,
+  onComplete : () =>{
+    bgTl.kill()
+  }
+})
+
+
+
+
 
 }
 
