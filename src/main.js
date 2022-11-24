65518b719ba8e39e3ca48feda691cef05fb60fab
ledez gregory <ledez.greg@gmail.com>
2022-11-24 14:56:49 +0100

    background moving, sprite ok, obstacle ok

--- src/main.js
+++ src/main.js
@@ -1,3 +1,41 @@
+import { gsap } from "gsap";
 import './style.scss';
+
+const playBtn = document.querySelector('.playBtn');
+
+const bgTl = gsap.timeline();
+
+
+ 
+const play = () => {
+
+
+  bgTl.to('.levelOne', {
+    x:'-100%',
+    duration:10,
+    delay:1,
+    ease:'linear'
+
+  })
+.to('.obstacle', {
+  x:'-2000px',
+  duration:4
+}, 2)
+  .to('.levelTwo', {
+    x:'-200%',
+    duration:8,
+    ease:'linear'
+  }, 4)
+  bgTl.paused( !bgTl.paused());
+playBtn.innerHTML = bgTl.paused() ? 'Start' : 'Pause'
+
+}
+
+
+
+playBtn.addEventListener('click', play);
+
+
+
 
 
