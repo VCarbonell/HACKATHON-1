ab35fcc1bda5eb79e9bae3a0eb988f083d2527b0
VCarbonell <v.carbonell+gh@hotmail.fr>
2022-11-24 12:45:18 +0100

    wip

---
+++ src/collision.js
@@ -0,0 +1,5 @@
+const flag = document.querySelector('.flag');
+
+const flagPos = flag.getBoundingClientRect();
+
+console.log(flag.offset);
--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 import './style.scss';
+import './collision.js';
+
+
 
 
